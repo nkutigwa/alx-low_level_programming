49292f5cff988b09fc1775b9b9208714aac0044b
nkutigwa <nkw802640@gmail.com>
2022-03-10 04:36:58 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main -Entry point
  *
@@ -7,7 +6,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a  multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
-}
- 
+} 
