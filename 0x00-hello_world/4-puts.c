282464f5d063fa6fcdcb0a7aec5fa7fa0a2c2dca
nkutigwa <nkw802640@gmail.com>
2022-03-10 05:42:40 -0800

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 /**
  * main -Entry point
- *
- * Return: Always 0(success)
- */
+ * Return: Always 0(success)*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
-} 
+puts("\"Programming is like building a multilingual puzzle");
+return 0;} 
