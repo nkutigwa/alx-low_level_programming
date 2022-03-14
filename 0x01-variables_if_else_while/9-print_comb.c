e05f354ad435424ae7cffdc4b19996d323723401
nkutigwa <nkw802640@gmail.com>
2022-03-14 15:17:13 -0700

    updated task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,25 +6,32 @@
  */
 int main(void)
 
+
 {
 int number;
+
 
 for (number == 48; number <= 57; number ++)
 {
 putchar(number);
+
 
 if (number ==57)
 {
 break;
 }
 
+
 putchar(',');
 putchar(' ');
 }
 
+
 putchar('\n');
 
+
 return (0);
+
 
 }
 
