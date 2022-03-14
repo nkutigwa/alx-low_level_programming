54fc573642342b833e5565dd3e030e7601e18707
nkutigwa <nkw802640@gmail.com>
2022-03-14 13:10:06 -0700

    added changes alx C 0x01

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,13 +18,13 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 
-if(n==0)
+if (n == 0)
 {
 printf("%d is zero\n", n);
 }
 
 
-else if(n < 0)
+else if (n < 0)
 {
 printf("%d is negative\n", n);
 }
