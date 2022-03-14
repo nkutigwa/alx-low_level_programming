e0e535f96c3f847e93ac135cad0e7e36dc9f0b53
nkutigwa <nkw802640@gmail.com>
2022-03-14 14:06:27 -0700

    task 3 alx C 0x01

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,12 @@ int main(void)
 char lower_case;
 char upper_case;
 
-for (lower_case = 'a'; lower_case <= 'z'; upper_case++)
+for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
 putchar(lower_case);
 }
 
-for (upper_case = 'A'; upper_case <= 'Z'; lower_case++)
+for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 {
 putchar(upper_case);
 }
