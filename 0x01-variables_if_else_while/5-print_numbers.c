c40bf68c3f8d4967c7b9debce23ebc5144866496
nkutigwa <nkw802640@gmail.com>
2022-03-14 14:21:49 -0700

    task 5 alx C 0x01'

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+int digit;
+
+for (digit = 0; digit < 10; digit++)
+{
+printf ("%digit", digit);
+}
+
+pintf ("\n");
+
+return (0);
+
+}
