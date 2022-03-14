e12cda1415b58a1e0b6a805dcf8f1981405b4ffc
nkutigwa <nkw802640@gmail.com>
2022-03-14 14:48:55 -0700

    task 6 alx C 0x01

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+putchar (i);
+}
+
+putchar ('\n');
+
+return (0);
+
+}
+
