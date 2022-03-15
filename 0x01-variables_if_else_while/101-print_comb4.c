fa9d1e8dd048982a08485c50f8d664a29fe71d8d
nkutigwa <nkw802640@gmail.com>
2022-03-14 19:28:06 -0700

    updated task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,34 +6,46 @@
  */
 int main(void)
 
+
 {
 int digit1, digit2, digit3;
+
 
 for (digit1 = 0; digit1 < 8; digit1++)
 {
 
+
 for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 {
+
 
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
 putchar((digit3 % 10) + '0');
 
+
 if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 continue;
 
+
 putchar(',');
-putchar(' ')'
+putchar(' ');
+
 
 }
 
-}
 
 }
+
+
+}
+
 
 putchar('\n');
 
+
 return (0);
+
 
 }
 
