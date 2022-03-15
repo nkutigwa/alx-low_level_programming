2bf09c5909bab21139ac884ff08e13b339d3fe2f
nkutigwa <nkw802640@gmail.com>
2022-03-15 03:29:02 -0700

    updated task 104'

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints the firs 98 Fibonacci numbers, starting with, 
+ * main - Prints the firs 98 Fibonacci numbers, starting with
  *        1 and 2, separated by a comma followed by a space.
  * Return: Always 0.
  */
@@ -24,12 +24,12 @@ fib2_half1 = fib2 / 10000000000;
 fib1_half2 = fib1 % 10000000000;
 fib2_half2 = fib2 % 10000000000;
 
-for(count = 93; count < 99; count++)
+for (count = 93; count < 99; count++)
 {
 half1 = fib1_half1 + fib2_half1;
 half2 = fib1_half2 + fib2_half2;
 
-if(fib1_half2 + fib2_half2 > 9999999999)
+if (fib1_half2 + fib2_half2 > 9999999999)
 {
 half1 += 1;
 half2 %= 10000000000;
