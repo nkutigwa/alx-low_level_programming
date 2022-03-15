38c3972b7ef4c1e7b41520d2a93b62029f779d68
nkutigwa <nkw802640@gmail.com>
2022-03-14 19:52:14 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,7 @@ for (num2 = num1 + 1; num2 <= 99; num2++)
 putchar((num1 / 10) + '0');
 putchar((num1 % 10) + '0');
 putchar(' ');
-putchar((num2 / 10) + '0'):
+putchar((num2 / 10) + '0');
 putchar((num2 % 10) + '0');
 
 if (num1 == 98 && num2 == 99)
