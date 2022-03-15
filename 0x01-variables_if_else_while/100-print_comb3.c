e202c688f31ec4843ef4ec3e78ccb78dbe2bff15
nkutigwa <nkw802640@gmail.com>
2022-03-14 19:08:37 -0700

    updated task 10 alx C 0x01

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,7 +18,7 @@ for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
 
-if (digit1 == 8 && digit2 ==9)
+if (digit1 == 8 && digit2 == 9)
 continue;
 
 putchar(',');
