55a34c4cf824fb618bc285e810e78caebf69b30a
nkutigwa <nkw802640@gmail.com>
2022-03-17 09:27:32 -0700

    task 9 alx 0x04

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ * main - replace multiples fo 3 and 5.
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("%s", "FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("%s", "Fizz");
+
+		}
+		else if ( i % 5 == 0)
+		{
+		printf("%s", "Buzz");
+
+		}
+		else
+		{
+			printf("%d", i)
+		}
+		if ( i != 100)
+		{
+			printf(" ");
+		}
+
+	}
+	
+	printf("\n");
+
+	return (0);
+
+}
