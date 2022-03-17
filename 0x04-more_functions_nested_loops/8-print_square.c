c6ac0095139f43c2b677abf47d7ca566ab206fe9
nkutigwa <nkw802640@gmail.com>
2022-03-17 09:18:37 -0700

    task 8 alx 0x04

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_square - prints square fo size size.
+ * @size: size of square.
+ *
+ * Return; void.
+ */
+void print_square(int size)
+
+{
+int i, j;
+
+if (size <= 0)
+
+{
+_putchar('\n');
+
+}
+else
+{
+	for(i = 1; i<= size, i++)
+
+	{
+		_putchar('#');
+		for (j = 1; j < size; j++)
+		{
+			_putchar('#');
+
+		}
+
+	_putchar('\n');
+	}
+
+}
+
+}
+
