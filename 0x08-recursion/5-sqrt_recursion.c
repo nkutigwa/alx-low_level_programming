4eeb100f2fb42ec5d2077ab2494f23a38bb79d5d
nkutigwa <nkw802640@gmail.com>
2022-03-30 01:34:24 -0700

    updated task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,14 +9,15 @@ int actual_sqrt_recursion(int n, int i);
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-	{
-		return (-1);	
-        }
 
 
-        return (actual_sqrt_recursion(n, 0));
+if (n < 0)
+{
 
+return (-1);	
+}
+
+return (actual_sqrt_recursion(n, 0));
 
 }
 /**
@@ -28,15 +29,23 @@ int _sqrt_recursion(int n)
  */
 int actual_sqrt_recursion(int n, int i)
 {
-	if (i * i > n)
-	{
-		return (-1);
-	}
-	if ( i * i == n)
-	{
-		return (i);
-	}
 
-	return (actual_sqrt_recursion(n, i + 1));
+
+if (i * i > n)
+{
+
+return (-1);
+
+}
+
+
+if ( i * i == n)
+{
+
+return (i);
+
+}
+
+return (actual_sqrt_recursion(n, i + 1));
 
 }
