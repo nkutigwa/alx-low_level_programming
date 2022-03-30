1ed551504531872d49f0ed41acaec6ab3d96438d
nkutigwa <nkw802640@gmail.com>
2022-03-30 01:37:09 -0700

    updated task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -15,11 +15,12 @@ if (n < 0)
 {
 
 return (-1);	
+
 }
 
 return (actual_sqrt_recursion(n, 0));
-
 }
+
 /**
  * actual_sqrt_recursion - recurses to find the natural 
  * square root of a number
@@ -30,14 +31,12 @@ return (actual_sqrt_recursion(n, 0));
 int actual_sqrt_recursion(int n, int i)
 {
 
-
 if (i * i > n)
 {
 
 return (-1);
 
 }
-
 
 if ( i * i == n)
 {
