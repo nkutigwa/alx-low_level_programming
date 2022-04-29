aaf33f047799ffbfdbff924e4d0eae009ca2ac13
Vicent <51188125+nkutigwa@users.noreply.github.com>
2022-04-29 05:27:08 -0700

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,47 +1,35 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "holberton.h"
+#include <stdlib.h>
 
 /**
-  * binary_to_uint - Converts a binary number to an unsigned int
-  * @b: The binary string to converts
-  *
-  * Return: The positive number converted from a binary
-  */
+ * binary_to_uint - Convert a string representing a binary number to an
+ * unsigned int decimal value
+ * @b: The string containing the binary
+ *
+ * Return: 0 if string contains something other than 1 or 0, or
+ * if the string is NULL, return the decimal value on success
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len = 0, count = 0, sum = 0;
+	unsigned int i, len;
+	unsigned int decval;
 
+	i = len = decval = 0;
 	if (b == NULL)
 		return (0);
-
-	len = _strlen(b);
-	while (len--)
+	while (b[len] != '\0')
 	{
-		if (b[len] != 48 && b[len] != 49)
+		if (b[len] == '0' || b[len] == '1')
+			len++;
+		else
 			return (0);
-
-		if (b[len] == 49)
-			sum += 1 << count;
-
-		count++;
 	}
-
-	return (sum);
-}
-
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(const char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
+	while (i < len)
+	{
+		decval = decval << 1;
+		if (b[i] == '1')
+			decval += 1;
+		i++;
+	}
+	return (decval);
 }
