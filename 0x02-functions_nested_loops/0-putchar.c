59adb9708a0271fd83e2f9d89a73473b4eee2b7a
nkutigwa <nkw802640@gmail.com>
2022-08-30 23:41:56 -0700

    updating task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,24 @@
-#include "alx.h"
-#include <unistd.h>
+#include "holberton.h"
+
 /**
- * _putchar - Writes the character c to stdout
- * @c: The character to print
+ * main - entry point
  *
- * Return: On success 1.
- * On error, -1 is returned, and error is set appropriately.
+ * Description: Prints Holberton with _putchar
+ *
+ * Return: always 0
  */
-int _putchar(char c)
 
+int main(void)
 {
+	int i = 0;
+	char c;
+	char s[] = "Holberton\n";
 
-return (write(1, &c, 1));
-
+	while (i <= 9)
+	{
+		c = s[i];
+		_putchar(c);
+		i++;
+	}
+	return (0);
 }
