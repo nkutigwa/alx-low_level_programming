8ef0b860629e59dd8d62988ccfcf528ea35f9875
nkutigwa <nkw802640@gmail.com>
2022-08-31 00:30:32 -0700

    adding task 10'

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add -function that adds two numbers
+ *
+ * @u: first number
+ * @v: second number
+ * Return: a number
+ */
+int add(int u, int v)
+{
+	int sum;
+
+	sum = u + v;
+	return (sum);
+}
