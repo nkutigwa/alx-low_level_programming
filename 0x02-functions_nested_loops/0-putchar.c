a2b31ab95fc73cffe18fe45857df42c118dfc101
nkutigwa <nkw802640@gmail.com>
2022-08-30 23:59:26 -0700

    adding 0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch1;
+	char ch2;
+	char ch3;
+	char ch4;
+	char ch5;
+	char ch6;
+	char ch7;
+	char ch8;
+	char ch9;
+
+	ch1 = '_';
+	ch2 = 'p';
+	ch3 = 'u';
+	ch4 = 't';
+	ch5 = 'c';
+	ch6 = 'h';
+	ch7 = 'a';
+	ch8 = 'r';
+	ch9 = '\n';
+	_putchar(ch1);
+	_putchar(ch2);
+	_putchar(ch3);
+	_putchar(ch4);
+	_putchar(ch5);
+	_putchar(ch6);
+	_putchar(ch7);
+	_putchar(ch8);
+	_putchar(ch9);
+	return (0);
+}
